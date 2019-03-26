79d6c1f1ff3fd654ad152ddb87b527cf04a99ad8
takumikaka <qk2006qk@163.com>
2019-03-26 11:12:44 +0800

    update

--- lib/db.py
+++ lib/db.py
@@ -1,1 +1,34 @@
 # coding:UTF-8 -*-
+
+import pymysql
+import sys
+sys.path.append("..")
+from config.config import *
+
+class DB(object):
+    def __init__(self):
+        self.conn = pymysql.connect(db_host, db_user, db_passwd, db)
+        self.cur = self.conn.cursor()
+
+    def __del__(self):
+        self.cur.close()
+        self.conn.close()
+
+    def query(self, sql):
+        self.cur.execute(sql)
+        return self.cur.fetchall()
+
+    def exec(self, sql):
+        try:
+            self.cur.execute(sql)
+            self.conn.commit()
+        except Exception as e:
+            logging.info(str(e))
+            self.conn.rollback()
+
+    def check_title(self, name):
+        result = self.query("select * from title where name = '{0}';".format(name))
+        return True if result else False
+
+    def add_title(self, name, url):
+        self.exec("insert into title(name, url) values('{0}', '{1}');".format(name, url))
---
+++ run.py
@@ -0,0 +1,12 @@
+# coding:UTF-8 -*-
+
+from lib.db import DB
+
+
+class Run(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        title_dict = get_dict()
+        
